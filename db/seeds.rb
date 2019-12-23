ced8380f39ff6bd87196a949b1d72986e05a8e3e
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-23 20:36:46 +1100

    Render all Challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 puts 'Generating Seeds'
 
 10.times do |challenge|
+  challenge += 1
   Challenge.create!(
     title: "Challenge ##{challenge}",
     gist: 'https://via.placeholder.com/500x600',
-    language: 'Ruby'
+    language: 'Ruby',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
   )
 end
 

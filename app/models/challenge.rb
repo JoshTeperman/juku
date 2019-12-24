a3948481944e58f4c156dd529ef3d486a253cc4d
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-24 16:13:01 +1100

    Add User FK to Challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,4 +2,5 @@ class Challenge < ApplicationRecord
   validates :title, :description, presence: true
 
   belongs_to :language
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_220505) do
+ActiveRecord::Schema.define(version: 2019_12_24_050250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2019_12_23_220505) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.bigint "language_id", null: false
+    t.bigint "user_id", null: false
     t.index ["language_id"], name: "index_challenges_on_language_id"
+    t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
   create_table "languages", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2019_12_23_220505) do
   end
 
   add_foreign_key "challenges", "languages"
+  add_foreign_key "challenges", "users"
 end

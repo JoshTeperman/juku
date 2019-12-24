22ce286c28f1c604b979a547262aaeaa8dad8f01
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-24 14:23:27 +1100

    sanitize name for Devise User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :whitelist_devise_paramaters, if: :devise_controller?
+
+  private
+
+  def whitelist_devise_paramaters
+    devise_paramater_sanitizer.permit(:sign_up, keys: [:username])
+    devise_paramater_sanitizer.permit(:account_update, keys: [:username])
+  end
 end

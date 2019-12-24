f7b6ca4367792a0aec76caca14ba8732579c9f4d
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-24 15:32:23 +1100

    add devise links to nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_filter :whitelist_devise_paramaters, if: :devise_controller?
+  before_action :whitelist_devise_paramaters, if: :devise_controller?
 
   private
 
   def whitelist_devise_paramaters
-    devise_paramater_sanitizer.permit(:sign_up, keys: [:username])
-    devise_paramater_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end

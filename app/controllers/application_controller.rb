f6c8e63569a54d1ba7634ad41d9e53d7aa53ae97
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-25 22:48:12 +1100

    Guest User and modified current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+
+  def current_user
+    super || guest_user
+  end
+
+  private
+
+  def guest_user
+    guest_user = Guest.new(email: "guest_#{Time.now.to_i}@guest.com", username: 'Guest')
+  end
 end

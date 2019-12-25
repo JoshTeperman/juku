6f12eb5471e6d772240b60fe6e9f20a20921c25b
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-25 19:20:50 +1100

    update solution and edit page

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -27,7 +27,34 @@ class SolutionsController < ApplicationController
           notice: 'Created new solution'
         end
       else
-        format.html { redirect_to new_challenge_solution_path(challenge), notice: "Failed to create new solution: #{solution.errors.full_messages}" }
+        format.html do
+          redirect_to new_challenge_solution_path(challenge),
+          notice: "Failed to create new solution: #{solution.errors.full_messages}"
+        end
+      end
+    end
+  end
+
+  def edit
+    @challenge = Challenge.find(params[:challenge_id])
+    @solution = Solution.find(params[:id])
+  end
+
+  def update
+    challenge = Challenge.find(params[:challenge_id])
+    solution = Solution.find(params[:id])
+
+    respond_to do |format|
+      if solution.update(solution_params)
+        format.html do
+          redirect_to challenge_solution_path(challenge_id: challenge.id, id: solution.id),
+          notice: 'Updated solution'
+        end
+      else
+        format.html do
+           redirect_to edit_challenge_solution_path(challenge_id: challenge.id, id: solution.id),
+           notice: "Failed to update solution: #{solution.errors.full_messages}"
+        end
       end
     end
   end
